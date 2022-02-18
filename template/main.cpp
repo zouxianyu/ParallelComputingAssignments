bcfa0ea80815869997be6d8d1c68a47b52e9ba6c
zouxianyu <2979121738@qq.com>
2022-02-18 21:36:15 +0800

    使用c++11标准std::chrono的时钟进行精确计时，可以跨平台

--- template/main.cpp
+++ template/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <chrono>
+#include <ratio>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+    auto start = std::chrono::steady_clock::now();
+
+    int temp;
+    for (int i = 0; i < 1000; ++i) {
+        temp = i;
+    }
+
+    auto end = std::chrono::steady_clock::now();
+
+    std::cout << std::chrono::duration <double, std::milli> (end - start).count() << " ms" << std::endl;
 }
