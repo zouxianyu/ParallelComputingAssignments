f9a755cbf62fb55b8b2c4b9c3cd78607d376c254
zouxianyu <2979121738@qq.com>
2022-02-24 20:10:41 +0800

    增加4路并行求和算法

--- assignment1_superscalar_optimization/main.cpp
+++ assignment1_superscalar_optimization/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <chrono>
 #include <ratio>
+#include <cassert>
+#include <numeric>
 
 const int N = 1 << 24;//2^24
 int32_t V[N];
@@ -28,7 +30,7 @@ void naive_sum() {
 /*
  * multiplexing algorithm
  */
-void multiplexing_sum() {
+void multiplexing2_sum() {
     int64_t s1 = 0;
     int64_t s2 = 0;
     S = 0;
@@ -37,6 +39,21 @@ void multiplexing_sum() {
         s2 += V[i + 1];
     }
     S = s1 + s2;
+}
+
+void multiplexing4_sum() {
+    int64_t s1 = 0;
+    int64_t s2 = 0;
+    int64_t s3 = 0;
+    int64_t s4 = 0;
+    S = 0;
+    for (int i = 0; i < N; i += 4) {
+        s1 += V[i];
+        s2 += V[i + 1];
+        s3 += V[i + 2];
+        s4 += V[i + 3];
+    }
+    S = s1 + s2 + s3 + s4;
 }
 
 /*
@@ -63,6 +80,7 @@ void test(void (*func)(), int n) {
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double, std::milli> elapsed = end - start;
     std::cout << "avg time: " << elapsed.count() / n << " ms" << std::endl;
+    assert(S==std::accumulate(V, V + N, 0));
 }
 
 /*
@@ -75,11 +93,14 @@ int main(int argc, char **argv) {
         return 1;
     }
 
+
     //check the method argument
     if (std::string(argv[1]) == "naive") {
         test(naive_sum, atol(argv[2]));
-    } else if (std::string(argv[1]) == "mul") {
-        test(multiplexing_sum, atol(argv[2]));
+    } else if (std::string(argv[1]) == "mul2") {
+        test(multiplexing2_sum, atol(argv[2]));
+    } else if (std::string(argv[1]) == "mul4") {
+        test(multiplexing4_sum, atol(argv[2]));
     } else if (std::string(argv[1]) == "rec") {
         test(recursive_sum, atol(argv[2]));
     } else {
