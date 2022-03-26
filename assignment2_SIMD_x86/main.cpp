e304d7ae21e181aa515f005c58fc56ec9a26d170
zouxianyu <2979121738@qq.com>
2022-03-26 20:55:57 +0800

    新建SIMD x86项目

---
+++ assignment2_SIMD_x86/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <chrono>
+#include <ratio>
+
+
+
+int main() {
+
+    auto start = std::chrono::steady_clock::now();
+
+    int temp;
+    for (int i = 0; i < 1000; ++i) {
+        temp = i;
+    }
+
+    auto end = std::chrono::steady_clock::now();
+
+    std::cout << std::chrono::duration <double, std::milli> (end - start).count() << " ms" << std::endl;
+}
